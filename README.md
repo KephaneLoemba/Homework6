a74729571a5562cc42893d5f0a8090e5ef40bd70
dentyne icebourg <kloem058@gmail.com>
2020-03-22 23:57:33 -0400

    added most HTML elements to the inex file. added CSS modifications.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Homework6
 This week we are building a weather report application.
 
-Here is the link to my Application: https://kephaneloemba.github.io/Homework6/
+Here is the link to my [Weather-Application] (https://kephaneloemba.github.io/Homework6/).
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // This is my scipt.
+
+
+
+
+let APIKey = 'fdd4402a2a16fa66299bd0a6a4043864'
+let cityName = 'Ottawa'
+
+      // Here we are building the URL we need to query the database
+      let queryURL =
+        'https://api.openweathermap.org/data/2.5/weather?q='
+         + cityName + 
+         '&appid=' +
+        APIKey
+
+
+        fetch(queryURL)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                console.log(data)
+            })
