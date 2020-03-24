5c048dcbf8be9def37c6d2d42fe1c48c3e11ba50
dentyne icebourg <kloem058@gmail.com>
2020-03-24 00:49:58 -0400

    Buttons are now dinamically generated. Created seperate functions, to render weather of selected city.

--- script.js
+++ script.js
@@ -1,20 +1,79 @@
 // This is my scipt.
 
 
+let previousCity
+let newCity
+let buttonList = []
+let addCityButton = document.getElementById('search-button')
+
+addCityButton.addEventListener('click', createCityButton);
+
+renderButtonHistory();
+
+function createCityButton(){
+
+    newCity = document.getElementById('search-input').value
+    console.log(newCity)
+    newCity = newCity.trim();
+
+    if (newCity === "") {
+        alert("enter a city, and try again!")
+    }
+    else if (buttonList.includes(newCity)) {
+        alert("This city is allready among your search history. Add a different one! :)")
+    }
+    else {
+        buttonList.push(newCity)
+        console.log("a new city has been pushed!")
+        console.log(buttonList)
+
+        renderButtonHistory()
+    }
+
+
+}
+
+function renderButtonHistory() {
+    if (buttonList.length > 0) {
+
+        let historyDiv = document.getElementById('buttons-div')
+
+        historyDiv.textContent = ''
+
+        for (let i = 0; buttonList.length > i; i++) {
+        
+        let newButton = document.createElement('button')
+        newButton.classList.add('btn', 'btn-light', 'btn-lg', 'btn-block')
+        newButton.setAttribute('data-city', buttonList[i]);
+        newButton.innerHTML = buttonList[i]
+
+        newButton.addEventListener('click', renderCurrentWeather)
+
+        
+        historyDiv.appendChild(newButton)
+        console.log("button should be appended, now!")
+        
+        }
+
+    }
+}
+
+
 function renderCurrentWeather() {
+    console.log(this.getAttribute('data'))
 
 
 let APIKey = 'fdd4402a2a16fa66299bd0a6a4043864'
-let cityName = 'ottawa'
+let cityName = this.getAttribute('data-city') || previousCity
 
       // Here we are building the URL we need to query the database
       let queryURL =
         'https://api.openweathermap.org/data/2.5/weather?q='
          + cityName + 
          '&appid=' +
-        APIKey
+        APIKey  
 
-
+        console.log(cityName)
 
         fetch(queryURL)
             .then(function(response){
@@ -26,7 +85,7 @@ let cityName = 'ottawa'
                 console.log(data.wind.speed)
                 console.log(data.weather[0].icon)
                 let temps = data.main.temp-273.15;
-                document.getElementById('icon-day-1').previousSibling.textContent = " (" + moment().format("L") + ") ";
+                document.getElementById('icon-day-1').previousSibling.textContent = cityName + " (" + moment().format("L") + ") ";
                 document.getElementById('icon-day-1').src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
                 document.getElementById('temp').textContent = temps.toFixed(2)
                 document.getElementById('humid').textContent = data.main.humidity
@@ -57,5 +116,5 @@ let cityName = 'ottawa'
 
 }            
 
-renderCurrentWeather();
+
             // console.log(document.getElementById('search-button').textcontent)
